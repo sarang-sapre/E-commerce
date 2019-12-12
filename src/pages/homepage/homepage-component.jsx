3aaaf59a743e1751cb22e08df4371549f776643b
sarang-sapre <57029965+sarang-sapre@users.noreply.github.com>
2019-12-12 22:36:48 +0530

    secound day

---
+++ src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.styles.scss';
+import '../../component/menu-item/menu-item.component'
+import MenuItem from '../../component/menu-item/menu-item.component';
+import Directory from '../../component/directory-container/directory-container.component.jsx'
+
+const HomePage = () => (
+
+    <div className = 'homepage'>
+        <Directory />
+        </div>
+)
+
+
+export default HomePage;
